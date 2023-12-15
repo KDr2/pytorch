487606d73bb36bb31e74470025ff25f1bb3a0aaa
Yanbo Liang <ybliang8@gmail.com>
2023-12-15 15:20:03 -0800

    Update

--- torch/_dynamo/variables/misc.py
+++ torch/_dynamo/variables/misc.py
@@ -748,24 +748,6 @@ class SkipFilesVariable(VariableTracker):
 
         if inspect.getattr_static(self.value, "_torchdynamo_disable", False):
             unimplemented(f"call torch._dynamo.disable() wrapped function {self.value}")
-        # Allowlist a few popular classes(e.g, collections.OrderedDict) calls in skip files.
-        # elif self.value is collections.OrderedDict:
-        #     return BuiltinVariable.call_custom_dict(
-        #         tx, collections.OrderedDict, *args, **kwargs
-        #     )
-        # elif (
-        #     self.value is collections.defaultdict
-        #     and len(args) <= 1
-        #     and DefaultDictVariable.is_supported_arg(args[0])
-        # ):
-        #     return DefaultDictVariable(
-        #         {},
-        #         collections.defaultdict,
-        #         args[0],
-        #         mutable_local=MutableLocal(),
-        #     )
-        # Fold through the functions(e.g, collections.namedtuple)
-        # that inputs & outputs are all python constants
         elif (
             self.value in self.fold_through_function_to_wrapper().keys()
             and check_constant_args(args, kwargs)
@@ -777,6 +759,25 @@ class SkipFilesVariable(VariableTracker):
             return self.fold_through_function_to_wrapper().get(self.value)(
                 value, mutable_local=MutableLocal()
             )
+        elif (
+            self.value is functools.wraps
+            and not kwargs
+            and len(args) == 1
+            and (
+                args[0].source is not None or args[0].can_reconstruct(tx.output.root_tx)
+            )
+        ):
+
+            def wraps(fn):
+                if isinstance(fn, variables.NestedUserFunctionVariable):
+                    if args[0].source:
+                        reconstructible = args[0].source
+                    else:
+                        reconstructible = args[0]
+                    return fn.clone(wrapped_reconstructible=reconstructible)
+                unimplemented(f"functools.wraps({fn})")
+
+            return variables.LambdaVariable(wraps)
         elif (
             self.value is itertools.product
             and not kwargs
@@ -909,44 +910,6 @@ class SkipFilesVariable(VariableTracker):
                     "Unexpected failure when calling itertools.groupby"
                 )
             return variables.ListIteratorVariable(result, mutable_local=MutableLocal())
-        # elif (
-        #     self.value is functools.wraps
-        #     and not kwargs
-        #     and len(args) == 1
-        #     and (
-        #         args[0].source is not None or args[0].can_reconstruct(tx.output.root_tx)
-        #     )
-        # ):
-
-        #     def wraps(fn):
-        #         if isinstance(fn, variables.NestedUserFunctionVariable):
-        #             if args[0].source:
-        #                 reconstructible = args[0].source
-        #             else:
-        #                 reconstructible = args[0]
-        #             return fn.clone(wrapped_reconstructible=reconstructible)
-        #         unimplemented(f"functools.wraps({fn})")
-
-        #     return variables.LambdaVariable(wraps)
-        # elif self.value is collections.deque and not kwargs:
-        #     if len(args) == 0:
-        #         items = []
-        #     elif len(args) == 1 and args[0].has_unpack_var_sequence(tx):
-        #         items = args[0].unpack_var_sequence(tx)
-        #     else:
-        #         unimplemented("deque() with more than 1 arg not supported")
-        #     return variables.lists.DequeVariable(items, mutable_local=MutableLocal())
-        # elif self.value is functools.partial:
-        #     if not args:
-        #         unimplemented("functools.partial malformed")
-        #     # The first arg, a callable (the ctor below will assert on types)
-        #     fn = args[0]
-        #     rest_args = args[1:]
-        #     # guards for the produced FunctoolsPartialVariable are installed in FunctoolsPartialVariable ctor from the
-        #     # args and keywords
-        #     return variables.functions.FunctoolsPartialVariable(
-        #         fn, args=rest_args, keywords=kwargs
-        #     )
         elif self.value is itertools.repeat:
             if len(args) < 2:
                 return variables.RepeatIteratorVariable(
@@ -970,24 +933,6 @@ class SkipFilesVariable(VariableTracker):
             msg = f"'skip function {self.value.__qualname__} in file {path}'"
             msg += f"', {self.reason}'" if self.reason else ""
             unimplemented(msg)
-
-    # def call_method(
-    #     self,
-    #     tx,
-    #     name,
-    #     args: "List[VariableTracker]",
-    #     kwargs: "Dict[str, VariableTracker]",
-    # ) -> "VariableTracker":
-    #     if (
-    #         self.value in {collections.OrderedDict, collections.defaultdict}
-    #         and name == "fromkeys"
-    #     ):
-    #         from .builtin import BuiltinVariable
-
-    #         return BuiltinVariable.call_custom_dict_fromkeys(
-    #             tx, self.value, *args, **kwargs
-    #         )
-    #     return super().call_method(tx, name, args, kwargs)
 
 
 class TypingVariable(VariableTracker):
