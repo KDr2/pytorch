f4b20588866eb4ad5298dfe801d2065f7f3e67a6
Wenting Wang <wentingwang@meta.com>
2024-03-11 15:52:45 -0700

    [torch export] replace traceback.extract_stack with CapturedTraceback.extract (#121449)
    Summary:
    
    with a simple bench in TestDeserializer.test_basic function: 
    ```
    time_start = time.time()
    for i in range(1000):
        self.check_graph(MyModule(), inputs)
    warnings.warn(f"time_taken: {time.time() - time_start}")
    ```
    and forcing FakeTensorConfig.debug to True, record_stack_traces to True, logging level to debug, it shows that the the changed code is consistently ard 20 secs faster (~90s vs originally ~110s)
    
    Test Plan:
    test passed, see summary
    
    compared debug trace before and after: 
    - exactly the same for fake tensor and proxy callsite https://www.internalfb.com/intern/diffing/?paste_number=1189883685
    - slightly different for the user frame in proxy node https://www.internalfb.com/intern/diffing/?paste_number=1189884347
    
    Reviewed By: angelayi
    
    Differential Revision: D54237017

--- torch/_subclasses/fake_tensor.py
+++ torch/_subclasses/fake_tensor.py
@@ -35,7 +35,7 @@ from torch.utils._python_dispatch import (
     is_traceable_wrapper_subclass,
     TorchDispatchMode,
 )
-
+from torch.utils._traceback import CapturedTraceback
 from torch.utils._pytree import PyTree, tree_map
 from torch.utils._stats import count
 from torch.utils.weak import WeakIdRef
@@ -499,11 +499,8 @@ class FakeTensor(torch.Tensor):
         self.constant = constant  # type: ignore[attr-defined]
         self._nonzero_memo = None  # type: ignore[attr-defined]
         self._nonzero_memo_vc = None  # type: ignore[attr-defined]
-
         if FakeTensorConfig.debug:
-            import traceback
-
-            self._debug_trace = traceback.extract_stack()  # type: ignore[attr-defined]
+            self._debug_trace = CapturedTraceback.extract()  # type: ignore[attr-defined]
         return self
 
     # In some circumstances, a conventional torch.Tensor constructor
