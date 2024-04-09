5b4c6ae23cd0116115db8fa8544eafe8910549c5
Pian Pawakapan <pianpwk@meta.com>
2024-04-09 15:25:45 -0700

    [export] set AOTAutograd ctx to enable_grad with pre-dispatch export (#123671)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/123671
    
    Currently, torch.export (through AOTAutograd) compiles with a torch.no_grad() wrapper, which affects the presence of `set_grad_enabled` nodes in pre-dispatch export graphs. This changes the wrapper to nullcontext (i.e. enable grad) if `pre_dispatch=True`.
    
    An example that previously failed without `with torch.no_grad()` is below:
    
    ```
    class Model(torch.nn.Module):
        def forward(self, x, y):
            with torch.enable_grad():
                x = x + y
            return x
    
    model = Model()
    exported_program = torch.export._trace._export(
        model,
        (torch.tensor(2), torch.tensor(3)),
        dynamic_shapes=None,
        pre_dispatch=True,
        strict=False
    )
    ```
    
    The pass would inline the add call, but then try to construct a HOO subgraph with no inputs/outputs:
    ```
    def forward(self):
        _set_grad_enabled_1 = torch._C._set_grad_enabled(False)
    ```
    
    Test Plan: Test case checking that nullcontext & no_grad wrappers lead to different export behaviors (regarding set grad subgraphs).
    
    Reviewed By: tugsbayasgalan
    
    Differential Revision: D55777804

--- test/export/test_export.py
+++ test/export/test_export.py
@@ -3667,22 +3667,6 @@ def forward(self, p_bar_linear_weight, p_bar_linear_bias, x):
         gm_unflat_strict = unflatten(ep)
 
     def test_predispatch_cond(self):
-        class Model(torch.nn.Module):
-            def forward(self, x, y):
-                with torch.enable_grad():
-                    x = x - y
-                return x + y
-
-        model = Model()
-        with torch.no_grad():
-            exported_program = torch.export._trace._export(
-                model,
-                (torch.tensor(10), torch.tensor(12)),
-                {},
-                dynamic_shapes=None,
-                pre_dispatch=True,
-                strict=False
-            )
 
         class Model(torch.nn.Module):
             def __init__(self):
@@ -3733,6 +3717,50 @@ def forward(self, arg1_1, arg0_1, arg2_1):
     add = torch.ops.aten.add.Tensor(sub, arg0_1);  sub = arg0_1 = None
     add_1 = torch.ops.aten.add.Tensor(add, arg2_1);  add = arg2_1 = None
     return add_1""")
+
+    def test_predispatch_grad_wrappers(self):
+        class Model(torch.nn.Module):
+            def forward(self, x, y):
+                with torch.enable_grad():
+                    x = x - y
+                with torch.no_grad():
+                    x = x + y
+                return x
+
+        # no grad
+        model = Model()
+        with torch.no_grad():
+            ep_nograd = torch.export._trace._export(
+                model,
+                (torch.tensor(10), torch.tensor(12)),
+                {},
+                dynamic_shapes=None,
+                pre_dispatch=True,
+                strict=False
+            )
+        # check that only sub op is wrapped with grad_enabled
+        getattr_nodes = [node for node in ep_nograd.graph.nodes if node.op == "get_attr"]
+        self.assertEqual(len(getattr_nodes), 1)
+        grad_subgraph = getattr(ep_nograd.graph_module, getattr_nodes[0].target)
+        op_node = [node for node in grad_subgraph.graph.nodes if node.op == "call_function"][0]
+        self.assertEqual(op_node.target._name, "aten::sub.Tensor")
+
+        # enable grad
+        model = Model()
+        ep_grad = torch.export._trace._export(
+            model,
+            (torch.tensor(10), torch.tensor(12)),
+            {},
+            dynamic_shapes=None,
+            pre_dispatch=True,
+            strict=False
+        )
+        # check that only add op is wrapped with grad_enabled
+        getattr_nodes = [node for node in ep_grad.graph.nodes if node.op == "get_attr"]
+        self.assertEqual(len(getattr_nodes), 1)
+        grad_subgraph = getattr(ep_grad.graph_module, getattr_nodes[0].target)
+        op_node = [node for node in grad_subgraph.graph.nodes if node.op == "call_function"][0]
+        self.assertEqual(op_node.target._name, "aten::add.Tensor")
 
 
     def test_non_persistent_buffer(self):
