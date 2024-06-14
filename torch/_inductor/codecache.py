40252333132f9dbbf04deca9b71efd7167711cee
Han, Xu <xu.han@intel.com>
2024-06-14 16:20:01 +0800

    Revert "use std::alignas"
    This reverts commit d456455a276a276374e941be5df0fc3e6246b253.

--- torch/_inductor/codecache.py
+++ torch/_inductor/codecache.py
@@ -1302,7 +1302,15 @@ class VecISA:
 #include <ATen/cpu/vec/vec.h>
 #endif
 
-std::alignas float in_out_ptr0[16] = {0.0};
+#if defined(__GNUC__)
+#define __at_align__ __attribute__((aligned(64)))
+#elif defined(_WIN32)
+#define __at_align__ __declspec(align(64))
+#else
+#define __at_align__
+#endif
+
+__at_align__ float in_out_ptr0[16] = {0.0};
 
 extern "C" void __avx_chk_kernel() {
     auto tmp0 = at::vec::Vectorized<float>(1);
