102788ef5f5f902ba475ecef5700b8a946b5b9e1
Rachel Guo <guorachel@meta.com>
2024-07-31 13:49:07 -0700

    [AOTI][tooling][1/n] Add intermediate value debug printer (#132323)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/132323
    
    **Context:**
    
    Currently we have a helper to print out AtenTensor in [shim_common.cpp](https://github.com/pytorch/pytorch/blob/v2.4.0-rc4/torch/csrc/inductor/aoti_torch/shim_common.cpp#L866)
    
    The way we were using this function was a “manual” process. We inject this function into the generated output.cpp file, and recompile and reload the file. This diff automates the printing value process.
    
    **Changes:**
    
    1. Added a simple initial debug printer helper to print out tensor values
    
    2. Added a filter option to selectively dump tensor values.
    
    **Usage:**
    
    Sample cmd :
    
    ```
    AOT_INDUCTOR_DEBUG_INTERMEDIATE_VALUE_PRINTER=1 TORCHINDUCTOR_FORCE_DISABLE_CACHES=1  TORCHINDUCTOR_ABI_COMPATIBLE=1 TORCH_COMPILE_DEBUG=1 TORCH_LOGS="+inductor, +schedule, output_code"  python test/inductor/test_aot_inductor.py -k test_addmm_abi_compatible_cuda
    ```
    
    Sample outputs :
    ```
    [  before_launch - triton_poi_fused_0 - buf0  ]:
     0.6331
     1.6358
    -0.3459
     1.0196
    -0.4122
     1.4279
    [ CUDAFloatType{6} ]
    Min value: -0.412198
    Max value: 1.63582
    Device: cuda:0
    Size: [6]
    Stride: [1]
    Dtype: float
    Layout: Strided
    Number of elements: 6
    Is contiguous: 1
    Requires grad: 0
    
    [  after_launch - triton_poi_fused_0 - buf0  ]:
     0.6331
     1.6358
    -0.3459
     1.0196
    -0.4122
     1.4279
    [ CUDAFloatType{6} ]
    Min value: -0.412198
    Max value: 1.63582
    Device: cuda:0
    Size: [6]
    Stride: [1]
    Dtype: float
    Layout: Strided
    Number of elements: 6
    Is contiguous: 1
    Requires grad: 0
    
    [ before_launch - aoti_torch_cuda_addmm_out - buf1  ]:
    Min value: -2.25655
    Max value: 2.32996
    Device: cuda:0
    Size: [16, 6]
    Stride: [6, 1]
    Dtype: float
    Layout: Strided
    Number of elements: 96
    Is contiguous: 1
    Requires grad: 0
    
    [  before_launch - aoti_torch_cuda_addmm_out - buf0  ]:
     0.6331
     1.6358
    -0.3459
     1.0196
    -0.4122
     1.4279
    [ CUDAFloatType{6} ]
    Min value: -0.412198
    Max value: 1.63582
    Device: cuda:0
    Size: [6]
    Stride: [1]
    Dtype: float
    Layout: Strided
    Number of elements: 6
    Is contiguous: 1
    Requires grad: 0
    
    [  after_launch - aoti_torch_cuda_addmm_out - buf1  ]:
    Min value: -12.0839
    Max value: 11.6878
    Device: cuda:0
    Size: [16, 6]
    Stride: [6, 1]
    Dtype: float
    Layout: Strided
    Number of elements: 96
    Is contiguous: 1
    Requires grad: 0
    
    [  after_launch - aoti_torch_cuda_addmm_out - buf0  ]:
     0.6331
     1.6358
    -0.3459
     1.0196
    -0.4122
     1.4279
    [ CUDAFloatType{6} ]
    Min value: -0.412198
    Max value: 1.63582
    Device: cuda:0
    Size: [6]
    Stride: [1]
    Dtype: float
    Layout: Strided
    Number of elements: 6
    Is contiguous: 1
    Requires grad: 0
    
    stats [('calls_captured', 1), ('unique_graphs', 1)]
    inductor [('pattern_matcher_count', 2), ('pattern_matcher_nodes', 2), ('extern_calls', 2)]
    .
    ----------------------------------------------------------------------
    Ran 1 test in 10.867s
    
    OK
    
    ```
    
    The user is able to filter kernel names to print out values by specifying env var `AOT_INDUCTOR_FILTERED_KERNELS_TO_PRINT` and see choices of kernel names in a log message like below:
    ```
    torch/_inductor/graph.py:1642] Finished codegen for all nodes. The list of kernel names available: ['triton_poi_fused_0', 'aoti_torch_cuda_addmm_out']
    
    ```
    
    In the follow-up diff, will add `torch.save()` to dump/save the intermediate tensors into individual `.pt` files that can be further  `torch.load()`.
    
    Test Plan:
    Run Unit Tests in OSS: (similar cmd as mentioned above in the usage part)
    
     `AOT_INDUCTOR_DEBUG_INTERMEDIATE_VALUE_PRINTER=1 TORCHINDUCTOR_FORCE_DISABLE_CACHES=1  TORCHINDUCTOR_ABI_COMPATIBLE=1 TORCH_COMPILE_DEBUG=1 TORCH_LOGS="+inductor, output_code"  python test/inductor/test_aot_inductor.py -k test_addmm_abi_compatible_cuda`
    
    Differential Revision: D60538496

--- torch/_inductor/graph.py
+++ torch/_inductor/graph.py
@@ -62,6 +62,7 @@ from .codegen.common import (
     get_wrapper_codegen_for_device,
     init_backend_registration,
 )
+from .codegen.debug_utils import DebugPrinter
 from .exc import (
     CppWrapperCodeGenError,
     LoweringException,
@@ -247,7 +248,7 @@ def mark_nodes_dislike_padding(g: Graph) -> None:
                     prior.meta["dislike_padding"] = True
 
 
-class GraphLowering(torch.fx.Interpreter):
+class GraphLowering(torch.fx.Interpreter):  # pyre-ignore[13]
     graph_outputs: List[ir.IRNode]
 
     def symbolic_sizes_strides(
@@ -272,13 +273,13 @@ class GraphLowering(torch.fx.Interpreter):
             # create_symbolic_sizes_strides_storage_offset but we hope we can
             # just delete this entirely
             source = ConstantSource(
-                f"__inductor_unknown_tensor_{len(self._shape_env.var_to_val)}"
+                f"__inductor_unknown_tensor_{len(self._shape_env.var_to_val)}" # pyre-ignore[16]
             )
             (
                 size,
                 stride,
                 _,
-            ) = self._shape_env.create_symbolic_sizes_strides_storage_offset(
+            ) = self._shape_env.create_symbolic_sizes_strides_storage_offset( # pyre-ignore[16]
                 ex,
                 source,
             )
@@ -441,6 +442,10 @@ class GraphLowering(torch.fx.Interpreter):
         self.effectful_ops: Dict[_EffectType, ir.Buffer] = {}
         self.aligned_inputs: Set[str] = set()
         self.no_fuse_buffer_names: Set[str] = set()
+
+        # Below fields are related to printing debug intermediate tensor values info for debugging
+        self.debug_printer = DebugPrinter()
+        self.all_codegen_kernel_names: List[str] = []
 
     def has_feature(
         self, device: Union[torch._inductor.ir.IRNode, device], feature: BackendFeature
@@ -1713,6 +1718,11 @@ class GraphLowering(torch.fx.Interpreter):
 
         self.wrapper_code.push_codegened_graph(self)
         self.scheduler.codegen()
+
+        # print out the codegened kernel_names in the second pass
+        if self.cpp_wrapper:
+            log.debug("Finished codegen for all nodes. The list of kernel names available: %s", V.graph.all_codegen_kernel_names)
+
         result = self.wrapper_code.generate(self.is_inference)
         self.wrapper_code.pop_codegened_graph()
         return result
