85b0ea51a746bf9907cce2bfd3cb4ec28bed1685
Menglu Yu <mengluy@meta.com>
2025-03-13 22:02:22 -0700

    [Inductor][Optimus] Add move view after cat aten pattern
    Summary:
    Add aten pattern to move the view/reshape out of split cat, further reduce the number of kernels.
    
    context: https://docs.google.com/document/d/1G2qFcQu1K7VXbz2uPe0CS2aBirnwtwI_B8lxmlBlAPQ/edit?tab=t.0
    
    Test Plan:
    ### how to enable
    Add the following patterns to the post grad
    ```
            post_grad_fusion_options={
                "normalization_aten_pass": {},
                "move_view_after_cat_aten_pass": {},
            },
    ```
    
    ### unit test
    ```
    buck2 test 'fbcode//mode/dev-nosan' fbcode//caffe2/test/inductor:split_cat_fx_aten_passes -- test_move_view_after_cat_aten
    ```
    
    Buck UI: https://www.internalfb.com/buck2/3c5451be-c63a-4794-8d6b-103ecac78905
    Test UI: https://www.internalfb.com/intern/testinfra/testrun/6192449704507267
    
    ### local reproduce
    
    ```
    buck2 run mode/opt scripts/shuaiyang:test -- --flow_id 691990503 --use_synthetic_data --optimus
    ```
    https://www.internalfb.com/intern/perfdoctor/trace_view?filepath=tree/traces/mengluy/2025-03-13-20-59-34/trace.json.gz&bucket=gpu_traces
    
    ### E2E
    
    baseline
    
    f691990503
    
    proposal
    
    Differential Revision: D71177004

--- torch/_inductor/fx_passes/split_cat.py
+++ torch/_inductor/fx_passes/split_cat.py
@@ -72,6 +72,7 @@ post_grad_pass_names = [
     "pad_aten_mm_pass",
     "split_cat_aten_pass",
     "select_cat_aten_pass",
+    "move_view_after_cat_aten_pass",
 ]
 
 for pass_name in pre_grad_pass_names:
@@ -2862,3 +2863,75 @@ def move_reshape_out_of_split_stack(match: Match, *args, **kwargs):
                 remove_split_unbind_children(graph, stack_inputs)  # type: ignore[arg-type]
                 remove_split_unbind_children(graph, split_users)  # type: ignore[arg-type]
             counters["inductor"]["move_reshape_out_of_split_stack_pass"] += 1
+
+
+view_getitem_split_aten = ListOf(
+    CallFunction(
+        [torch.ops.aten.reshape.default, torch.ops.aten.view.default],
+        CallFunction(
+            operator.getitem,
+            CallFunctionVarArgs(torch.ops.aten.split_with_sizes.default, users=MULTIPLE),
+            Ignored(),
+            _users=MULTIPLE,
+        ),
+        Arg(),
+        _users=MULTIPLE,
+    ),
+    partial=True,
+)
+
+
+@register_graph_pattern(
+    CallFunction(
+        torch.ops.aten.cat.default,
+        view_getitem_split_aten,
+        dim=Ignored(),
+        _users=MULTIPLE,
+    ),
+    pass_dict=construct_pattern_matcher_pass("move_view_after_cat_aten_pass"),
+)
+def move_view_after_cat(match: Match, *args, **kwargs):
+    split_node = next(node for node in match.nodes if node.target == torch.ops.aten.split_with_sizes.default)
+    split_input, split_section, split_dim = _get_split_args_default(split_node) 
+    split_users = list(split_node.users.keys())
+    getitem_indices = [getitem.args[1] for getitem in split_users if getitem.target == operator.getitem]
+    if not is_sorted_and_consecutive(getitem_indices):
+        return
+    cat_nodes = [node for node in match.nodes if node.target == torch.ops.aten.cat.default]
+    graph = match.graph
+    for cat_node in cat_nodes:
+        if not is_node_meta_valid(cat_node):
+            log.debug("example value absent for node: %s", cat_node)
+            continue
+        cat_dim = _get_dim(cat_node)
+        cat_inputs = get_arg_value(cat_node, 0, "tensors")  # type: ignore[union-attr]
+        # we only consider the following special case
+        if len(cat_inputs) != len(split_section):
+            continue
+        # for cat_input in cat_inputs:
+        view_indices =  [view.args[0].args[1] for view in cat_inputs]
+        if not is_sorted_and_consecutive(view_indices):
+            continue 
+        if cat_dim != split_dim:
+            # construct permute node
+            permute_list = list(range(len(cat_node.meta["val"].shape) + 1))
+            permute_list[split_dim], permute_list[cat_dim] = (
+                permute_list[cat_dim],
+                permute_list[split_dim],
+            )
+            permute_node = graph.call_function(
+                torch.ops.aten.permute.default,
+                args=(split_input, permute_list),
+            )
+        else:
+            permute_node = split_input
+
+        with graph.inserting_before(cat_node):
+            view_node = graph.call_function(
+                torch.ops.aten.reshape.default,
+                args=(permute_node, list(cat_node.meta["val"].shape))
+            )
+            cat_node.replace_all_uses_with(view_node)
+            view_node.meta.update(cat_node.meta)
+            graph.erase_node(cat_node)
+    counters["inductor"]["move_view_after_cat_aten_pass"] += 1
