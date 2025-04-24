65a373f329eae9b6911bdaa7a8f6d0ccd089f3be
Max Podkorytov <4273004+tenpercent@users.noreply.github.com>
2025-04-24 22:09:58 +0000

    add render

--- torch/_inductor/codegen/rocm/ck_tile_universal_gemm_template.py
+++ torch/_inductor/codegen/rocm/ck_tile_universal_gemm_template.py
@@ -296,144 +296,48 @@ class CKTileGemmTemplate(CKTileTemplate):
 #             template_params=(",\n" + 12 * " ").join(template_params),
 #         ), self._template_from_string(template_type).render(operation_name=op.name())
 
-#     def render(self, kernel: ROCmTemplateKernel, op: "CKGemmOperation", **kwargs) -> str:  # type: ignore[override]
-#         """
-#         The primary entry point for the code rendering process used in this template.
-#         """
-#         epilogue_nodes = kwargs.get("epilogue_nodes", None)
-#         assert epilogue_nodes is None or 0 == len(epilogue_nodes)
-#         template_buffer_node = kwargs.get("template_buffer_node", None)
-#         if template_buffer_node is not None:
-#             self.output_node = template_buffer_node
-#         X, W = self.input_nodes[0], self.input_nodes[1]
-#         Y = self.output_node
-#         Bias = self.input_nodes[2] if 3 == len(self.input_nodes) else None
+    def render(self, kernel: ROCmTemplateKernel, op: "CKTileGemmOperation", **kwargs) -> str:  # type: ignore[override]
+        """
+        The primary entry point for the code rendering process used in this template.
+        """
+        epilogue_nodes = kwargs.get("epilogue_nodes", None)
+        assert epilogue_nodes is None or 0 == len(epilogue_nodes)
+        template_buffer_node = kwargs.get("template_buffer_node", None)
+        if template_buffer_node is not None:
+            self.output_node = template_buffer_node
+        assert 2 == len(self.input_nodes)
+        X, W = self.input_nodes
+        Y = self.output_node
 
-#         op = copy.deepcopy(op)
+        instance_definition, instance_type = self.emit_ck_instance(op)
 
-#         # This parameter is converted into tuple because of change
-#         # from DeviceGemm_Xdl_CShuffleV3 to DeviceGemmMultiD_Xdl_CShuffle_V3.
-#         # The first tuple element corresponds to matmul result...
-#         op.c_shuffle_block_transfer_scalar_per_vector_n_per_block = (
-#             op.c_shuffle_block_transfer_scalar_per_vector_n_per_block,
-#         )
+        version_comment = rf"""/**
+* Generated code for CK inductor backend
+* See {type(self).__module__}.{type(self).__qualname__}
+*
+* Template instance {op}
+*
+* {torch.__version__=}
+* torch.version.git_version={getattr(torch.version, 'git_version', 'None')}
+*/
+"""
 
-#         if len(self.input_nodes) == 4:
-#             scale_x = self.input_nodes[2]
-#             scale_w = self.input_nodes[3]
-#             if 1 == scale_x.get_numel() and 1 == scale_w.get_numel():
-#                 op.c_elementwise_op = "Scale"
-#             else:
-#                 op.c_elementwise_op = "MultiplyMultiply"
-#                 op.c_shuffle_dtype = "F32"
-#                 op.ds_layouts = (
-#                     torch_layout_to_ck_layout(scale_x.get_layout()),
-#                     torch_layout_to_ck_layout(scale_w.get_layout()),
-#                 )
-#                 op.ds_element_dtypes = (
-#                     self._TORCH_DTYPE_TO_CK[scale_x.get_layout().dtype],
-#                     self._TORCH_DTYPE_TO_CK[scale_w.get_layout().dtype],
-#                 )
-#                 op.c_shuffle_block_transfer_scalar_per_vector_n_per_block += (1, 1)
-#         else:
-#             scale_x = None
-#             scale_w = None
-
-#         if Bias is not None:
-#             op.ds_layouts = (torch_layout_to_ck_layout(Bias.get_layout()),)
-#             op.ds_element_dtypes = ((self._TORCH_DTYPE_TO_CK[Bias.get_layout().dtype]),)
-#             op.c_elementwise_op = "Bilinear"
-#             # c_shuffle_dtype is also used for adding bias to matmul result
-#             # before converting down to the result dtype
-#             op.c_shuffle_dtype = op.acc_dtype
-#             # this parameter needs to be set accordingly to bias stride for correct accumulation
-#             if op.ds_layouts[0] == "Row":
-#                 # bias has (N, ) shape
-#                 bias_shuffle_block_transfer_scalar_per_vector_n_per_block = (
-#                     op.c_shuffle_block_transfer_scalar_per_vector_n_per_block
-#                 )
-#             else:
-#                 # bias has (M, 1) shape
-#                 bias_shuffle_block_transfer_scalar_per_vector_n_per_block = (1,)
-#             # ...and the second tuple element corresponds to the bias
-#             op.c_shuffle_block_transfer_scalar_per_vector_n_per_block += (
-#                 bias_shuffle_block_transfer_scalar_per_vector_n_per_block
-#             )
-
-#         instance_definition, instance_type = self.emit_ck_instance(op)
-
-#         version_comment = rf"""/**
-# * Generated code for CK inductor backend
-# * See {type(self).__module__}.{type(self).__qualname__}
-# *
-# * Template instance {op}
-# *
-# * {torch.__version__=}
-# * torch.version.git_version={getattr(torch.version, 'git_version', 'None')}
-# */
-# """
-#         epilogue = None
-
-#         if op.c_elementwise_op == "Bilinear":
-#             epilogue = f"Bilinear {{ {self.alpha}, {self.beta} }}"
-
-#         elif op.c_elementwise_op == "Scale":
-#             epilogue = "Scale { (inv_scale_w && inv_scale_x) ? (*inv_scale_w * *inv_scale_x) : 1.0f }"
-
-#         elif op.c_elementwise_op == "MultiplyMultiply":
-#             epilogue = "MultiplyMultiply {}"
-
-#         elif op.c_elementwise_op == "PassThrough":
-#             epilogue = "PassThrough {}"
-
-#         assert epilogue is not None, "CK GEMM epilogue is not set"
-
-#         return self._template_from_string(self.gemm_template).render(
-#             headers=self.header().getvalue(),
-#             globals=self.globals().getvalue(),
-#             instance_definition=instance_definition,
-#             kernel_definition=kernel.def_kernel(
-#                 inputs=[X, W, scale_x, scale_w, Bias],  # type: ignore[list-item]
-#                 outputs=[Y],
-#                 names_str="X, W, inv_scale_x, inv_scale_w, Bias, Y",
-#                 input_reorder=self.input_reorder,
-#                 size_args=[
-#                     f"int32_t {arg}"
-#                     for arg in ["M", "N", "K", "LDA", "LDB", "LDC", "LDD"]
-#                 ],
-#             ),
-#             instance_type=instance_type,
-#             a_element_dtype=op.a_element_dtype,
-#             b_element_dtype=op.b_element_dtype,
-#             c_element_dtype=op.c_element_dtype,
-#             bias_element_dtype=op.ds_element_dtypes[0] if Bias is not None else "",
-#             alpha=self.alpha,
-#             beta=self.beta,
-#             a_elementwise_op="PassThrough {}",
-#             b_elementwise_op="PassThrough {}",
-#             epilogue=epilogue,
-#             has_bias=Bias is not None,
-#             ds_size=1
-#             if Bias is not None
-#             else 2
-#             if op.c_elementwise_op == "MultiplyMultiply"
-#             else 0,
-#             ds_names=", ".join(
-#                 ["Bias"]
-#                 if Bias is not None
-#                 else ["inv_scale_x", "inv_scale_w"]
-#                 if op.c_elementwise_op == "MultiplyMultiply"
-#                 else []
-#             ),
-#             ds_strides=", ".join(
-#                 ["LDD"]
-#                 if Bias is not None
-#                 else ["0", "0"]
-#                 if op.c_elementwise_op == "MultiplyMultiply"
-#                 else []
-#             ),
-#             version_comment=version_comment,
-#         )
+        return self._template_from_string(self.gemm_template).render(
+            headers=self.header().getvalue(),
+            globals=self.globals().getvalue(),
+            instance_definition=instance_definition,
+            kernel_definition=kernel.def_kernel(
+                inputs=[X, W],  # type: ignore[list-item]
+                outputs=[Y],
+                names_str="X, W, Y",
+                size_args=[
+                    f"int32_t {arg}"
+                    for arg in ["M", "N", "K", "LDA", "LDB", "LDC"]
+                ],
+            ),
+            instance_type=instance_type,
+            version_comment=version_comment,
+        )
 
 #     def _is_rcr_f16(self):
 #         X_meta, W_meta, Y_meta = (
