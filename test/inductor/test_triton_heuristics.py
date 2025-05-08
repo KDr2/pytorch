baaa5d975de1c3a0037e335161c43dbc785559c2
Gabriel Ferns <gabeferns@meta.com>
2025-05-08 02:15:49 -0700

    lint

--- test/inductor/test_triton_heuristics.py
+++ test/inductor/test_triton_heuristics.py
@@ -158,8 +158,6 @@ class TestTritonHeuristics(TestCase):
         with self.assertRaisesRegex(AssertionError, "pre_hook"):
             CachingAutotuner(**args)
 
-
-
     def test_autotune_hints_to_configs(self):
         device_props = DeviceProperties.create(torch.device(GPU_TYPE))
         device_props = device_props._replace(warp_size=8)
