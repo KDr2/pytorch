07a02f3da3b395f250fa3d25a61a86ea93a08838
Shangdi Yu <shangdiy@meta.com>
2025-10-02 17:05:41 -0700

    mmap large weights on windows

--- torch/csrc/inductor/aoti_runtime/model_base.h
+++ torch/csrc/inductor/aoti_runtime/model_base.h
@@ -2,7 +2,7 @@
 #ifdef _WIN32
 #include <windows.h>
 #include <functional> // std::function
-#ifdef USE_MMAP_SELF
+#if defined(USE_MMAP_SELF) || defined(USE_MMAP_EXTERNAL)
 #include <errno.h>
 #include <fcntl.h>
 #include <io.h>
@@ -88,10 +88,18 @@ static DWORD get_creation_disposition(int flags) {
   return OPEN_EXISTING;
 }
 
+#ifndef O_ACCMODE
 #define O_ACCMODE 03
+#endif
+#ifndef O_RDONLY
 #define O_RDONLY 00
+#endif
+#ifndef O_WRONLY
 #define O_WRONLY 01
+#endif
+#ifndef O_RDWR
 #define O_RDWR 02
+#endif
 
 static DWORD get_access_mode(int flags) {
   switch (flags & O_ACCMODE) {
@@ -259,7 +267,7 @@ int munmap(void* addr, size_t length) {
   }
   return 0;
 }
-#endif // USE_MMAP_SELF
+#endif // defined(USE_MMAP_SELF) || defined(USE_MMAP_EXTERNAL)
 #else // !_WIN32
 #include <dlfcn.h>
 #include <sys/mman.h>
@@ -431,12 +439,14 @@ class AOTInductorModelBase {
       size_t num_constants,
       const std::string& device_str,
       std::optional<std::string> cubin_dir,
-      bool include_weights = true)
+      bool include_weights = true,
+      std::optional<std::string> weight_path = std::nullopt)
       : inputs_info_(num_inputs),
         outputs_info_(num_outputs),
         constants_info_(num_constants),
         cubin_dir_(std::move(cubin_dir)),
-        include_weights(include_weights) {
+        include_weights(include_weights),
+        external_weight_path_(std::move(weight_path)) {
     parse_device_str(device_str, device_type_, device_idx_);
 
 #ifdef USE_CUDA
@@ -904,8 +914,15 @@ class AOTInductorModelBase {
  protected:
   uint8_t* _get_constants_start() {
 #ifndef USE_MMAP_SELF
+#ifdef USE_MMAP_EXTERNAL
+  if (self_mmap) {
+    return self_mmap;
+  }
+  return _load_external_weights();
+#else
     // NOLINTNEXTLINE(*const-cast*)
     return const_cast<uint8_t*>(_binary_constants_bin_start);
+#endif
 #else
     if (self_mmap) {
       return self_mmap;
@@ -942,6 +959,35 @@ class AOTInductorModelBase {
     return self_mmap;
 #endif
   }
+
+#ifdef USE_MMAP_EXTERNAL
+  uint8_t* _load_external_weights() {
+
+  AOTI_RUNTIME_CHECK(external_weight_path_.has_value(),
+        "external_weight_path is not provided.");
+    
+    // The external_weight_path_ should be an absolute path as specified by the user
+    std::string weights_path = *external_weight_path_;
+    AOTI_RUNTIME_CHECK(!weights_path.empty(), "External weight path cannot be empty");
+
+    // Unix/Linux implementation using mmap
+    int fd = open(weights_path.c_str(), O_RDONLY);
+    AOTI_RUNTIME_CHECK(fd >= 0, "Failed to open external weights file: " + weights_path);
+
+    auto weights_size =
+        reinterpret_cast<const uint64_t*>(_binary_constants_bin_start)[0];
+
+    uint8_t* ptr = static_cast<uint8_t*>(mmap(
+        NULL, weights_size, PROT_READ, MAP_PRIVATE, fd, 0));
+
+    close(fd);
+    AOTI_RUNTIME_CHECK(ptr != MAP_FAILED, "mmap() failed");
+    self_mmap = static_cast<uint8_t*>(ptr);
+
+    return self_mmap;
+  }
+#endif 
+
   struct ParamInfo {
     const char* name = nullptr;
   };
@@ -973,7 +1019,8 @@ class AOTInductorModelBase {
   // Holds the blob storage for constants' at::Tensor.
   RAIIDataPtr constant_blob_;
 
-#ifdef USE_MMAP_SELF
+#if defined(USE_MMAP_SELF) || defined(USE_MMAP_EXTERNAL)
+  // Mapped memory for weights
   uint8_t* self_mmap = NULL;
 #endif
 
@@ -984,6 +1031,9 @@ class AOTInductorModelBase {
   // If True, we would prepare the weight when loading the model, otherwise the
   // model will be loaded without weights, and need to be provided by the user.
   bool include_weights;
+
+  // Path for external weights file
+  const std::optional<std::string> external_weight_path_;
 
   // Record if the model finishes an inference run so that its owning
   // AOTModelContainer can reuse this instance.
