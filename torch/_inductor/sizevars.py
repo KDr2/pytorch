0cff8444b32e9d80e587e88a87d914c70dc055aa
Pian Pawakapan <pianpwk@meta.com>
2025-10-06 16:40:26 -0700

    make backed_size_oblivious behavior consistent b/w symbolic_shapes/inductor (#164796)
    Summary:
    
    call guard_or_ directly to enable backed_size_obl in inductor calls to guard_or
    
    Test Plan: CI and unit test added.
    
    Reviewed By: laithsakka
    
    Differential Revision: D84009392

--- torch/_inductor/sizevars.py
+++ torch/_inductor/sizevars.py
@@ -453,9 +453,17 @@ class SizeVarAllocator:
     # Similar to the functions guard_or_false/guard_or_true in symbolic_shapes.py
     # but operates on sympy expressions instead of symnodes. see Note [guard_or_].
     def guard_or_false(self, left):
+        import torch.fx.experimental._config as exp_config
+
+        if exp_config.backed_size_oblivious:
+            return False
         return self.evaluate_expr(left, fallback_value=False)
 
     def guard_or_true(self, left):
+        import torch.fx.experimental._config as exp_config
+
+        if exp_config.backed_size_oblivious:
+            return True
         return self.evaluate_expr(left, fallback_value=True)
 
     # The evaluate functions evaluate some symbolic sympy expression
