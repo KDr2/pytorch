598483e82eb0ac48ff28aa3c57b24a894dc8d897
Deng, Daisy <daisy.deng@intel.com>
2025-11-11 08:03:09 +0000

    improve the msg for skipIfXpu decorectors

--- test/inductor/test_custom_lowering.py
+++ test/inductor/test_custom_lowering.py
@@ -208,7 +208,7 @@ class TestCustomLowering(InductorTestCase):
 
     @requires_gpu()
     @skipIfRocm
-    @skipIfXpu(msg="AssertionError: Tensor-likes are not close!")
+    @skipIfXpu(msg="https://github.com/intel/torch-xpu-ops/issues/2328")
     @skipIf(GPU_TYPE == "mps", "Not applicable to MPS")
     def test_tanh_approx(self):
         def fn(inp):
@@ -223,7 +223,7 @@ class TestCustomLowering(InductorTestCase):
 
     @requires_gpu()
     @skipIfRocm
-    @skipIfXpu(msg="AssertionError: Tensor-likes are not close!")
+    @skipIfXpu(msg="https://github.com/intel/torch-xpu-ops/issues/2328")
     @skipIf(GPU_TYPE == "mps", "Not applicable to MPS")
     def test_multi_inp_asm(self):
         def fn(a, b):
